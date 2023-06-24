c22b04fc20e8e829425f439dcf4e82eac5adaef2
Deepanshu Justa <deepanshujusta@gmail.com>
2023-06-24 23:22:49 +0530

    Static Index Page Design Completed

--- includes/header.php
+++ includes/header.php
@@ -7,21 +7,57 @@
     <title><?= $title ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <script src="https://kit.fontawesome.com/6e8d342420.js" crossorigin="anonymous"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@200;400;700&display=swap" rel="stylesheet">
 
-<style>
-    body {
-        font-family: 'Poppins', sans-serif;
-    }
-    .h-font {
-        font-family: 'Merienda', cursive;
-        font-weight: 700;
-    /* font-family: 'Poppins', sans-serif; */
-    }
-</style>
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .h-font {
+            font-family: 'Merienda', cursive;
+            font-weight: 700;
+            /* font-family: 'Poppins', sans-serif; */
+        }
+    </style>
 </head>
 
 <body>
+        <div class="bg-success text-white">
+            <div class="container d-none d-md-flex justify-content-between pt-3">
+                <p><i>Registered Under Tourism Department of Himachal Pradesh</i></p>
+                <p><b>Email:</b> info@yourtravellingpartner.com &nbsp; <b>Phone:</b> 70181-44110</p>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top border-3 border-bottom border-success">
+            <div class="container">
+                <a class="navbar-brand fs-2 h-font mx-auto" href="/">
+                    <span class="text-primary">Y</span>our <span class="text-primary">T</span>ravelling <span class="text-primary">P</span>artner</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">Packages</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
--- pages/home.php
+++ pages/home.php
@@ -6,6 +6,37 @@ require('includes/header.php');
 ?>
 
 <main>
+    <section id="banner">
+        <div class="container-fluid py-2 p-xl-4" style="background-image: url('assets/images/bg.png');">
+            <div class="card py-5" style="background-color: rgba(255,255,255,0.3);">
+                <p class="text-center mb-2 h-font fs-5 text-success">Explore the Himalayan Paradise with</p>
+                <h1 class="text-center h-font text-primary mb-4">Himachal Tour Packages</h1>
+                <p class="col-md-6 mx-auto text-center fw-bold">Experience the enchantment of Himachal Pradesh through our carefully curated tour packages. Explore majestic landscapes, embrace thrilling adventures, and immerse yourself in the rich culture of this Himalayan gem.</p>
+                <p class="text-center m-5"><a href="" class="btn btn-lg btn-primary rounded-pill h-font fs-3">Plan Your Tour Now!</a></p>
+            </div>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container my-5">
+            <div class="card shadow-lg">
+                <div class="row g-0">
+                    <div class="col-sm-6 mx-auto col-xl-5 order-2 order-xl-1">
+                        <img src="assets/images/collage.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-xl-7 order-1 order-xl-2">
+                        <div class="card-body">
+                            <h2 class="h1 h-font text-center text-xl-start">Welcome to <span class="text-primary">YTP Family</span></h2>
+                            <p class="card-text">YTP Family is not just a travel agency; it is a community of passionate adventurers, explorers, and dreamers. We believe that travel has the power to transform lives, create lasting memories, and connect people from all walks of life.</p>
+                            <p class="card-text">With our team of knowledgeable experts, we curate exclusive itineraries that showcase the tranquility of pristine mountain landscapes, the thrill of exhilarating outdoor adventures, and the immersive cultural experiences found in the vibrant destinations of Himachal Pradesh.</p>
+                            <p class="card-text">Join our vibrant community and embark on a journey of discovery, where every destination becomes a cherished memory. At YTP Family, we are more than a travel agency – we are your companions, your guides, and your family on the road less traveled. Let's explore the world together and create extraordinary experiences that will stay with you for a lifetime. Welcome to the YTP Family!</p>
+                            <p class="text-center mt-5"><a href="" class="btn btn-lg btn-outline-success m-2">Get In Touch</a><a href="" class="btn btn-lg btn-outline-primary m-2">Plan a Tour</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <section id="packages">
         <div class="container my-5">
@@ -14,13 +45,22 @@ require('includes/header.php');
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h3 class="card-title text-center h-font">Shimla</h3>
+                            <h3 class="h2 card-title text-center h-font text-primary">Shimla <sub class="text-success">2N/3D</sub></h3>
+                            <p class="h3 card-text d-flex justify-content-around"><i class="bi bi-building" title="Accommodation"></i><i class="bi bi-cup-hot"></i><i class="bi bi-taxi-front"></i><i class="bi bi-camera"></i><i class="bi bi-emoji-laughing"></i></p>
+                            <!-- <p class="h2 card-text text-success h-font">&#8377; 2,000/-</p> -->
+                            <p class="my-3">
+                                <a href="" class="btn btn-primary btn-lg">Get Free Quote</a>
+                                <a href="" class="btn btn-success btn-lg"><i class="bi bi-whatsapp"></i></a>
+                                <a href="" class="btn btn-primary btn-lg"><i class="bi bi-telephone-fill"></i></a>
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title text-center h-font">Manali</h3>
                         </div>
@@ -28,6 +68,7 @@ require('includes/header.php');
                 </div>
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title text-center h-font">Spiti</h3>
                         </div>
@@ -35,6 +76,7 @@ require('includes/header.php');
                 </div>
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title text-center h-font">Dharamshala</h3>
                         </div>
@@ -42,6 +84,7 @@ require('includes/header.php');
                 </div>
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title text-center h-font">Dalhousie</h3>
                         </div>
@@ -49,6 +92,7 @@ require('includes/header.php');
                 </div>
                 <div class="col">
                     <div class="card">
+                    <img src="assets/images/sml-1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title text-center h-font">Kinnaur</h3>
                         </div>
@@ -63,10 +107,10 @@ require('includes/header.php');
     <section id="destinations">
         <div class="container my-5">
             <div class="row">
-                <div class="col-lg-5">
-                    <div>
-                        <p class="h3 h-font text-success">YTP</p>
-                        <h2 class="h1 h-font">Popular <span class="text-primary">Destinations</span></h2>
+                <div class="col-lg-5 d-lg-flex align-items-center">
+                    <div class="">
+                        <p class="h3 h-font text-success text-center">Himachal Pradesh</p>
+                        <h2 class="h1 h-font text-center mb-4">Popular <span class="text-primary">Destinations</span></h2>
                         <p class="d-none d-sm-block">Himachal Pradesh, a haven nestled in the lap of the Himalayas, is home to a collection of popular destinations that leave visitors spellbound. From the charming colonial allure of Shimla to the adventurous allure of Manali, each place offers a unique blend of natural beauty, cultural richness, and thrilling experiences. Whether you're exploring the bustling streets of Shimla or indulging in adrenaline-pumping activities in Manali, Himachal Pradesh promises an unforgettable journey filled with breathtaking landscapes and warm hospitality. Get ready to embark on a remarkable adventure and create lasting memories in this picturesque region of India.</p>
                     </div>
                 </div>
@@ -74,6 +118,7 @@ require('includes/header.php');
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Shimla.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Shimla</h3>
                                 </div>
@@ -81,6 +126,7 @@ require('includes/header.php');
                         </div>
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Manali.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Manali</h3>
                                 </div>
@@ -88,6 +134,7 @@ require('includes/header.php');
                         </div>
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Spiti.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Spiti</h3>
                                 </div>
@@ -95,6 +142,7 @@ require('includes/header.php');
                         </div>
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Dharamshala.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Dharamshala</h3>
                                 </div>
@@ -102,6 +150,8 @@ require('includes/header.php');
                         </div>
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Dalhousie.jpg" class="card-img-top" alt="...">
+
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Dalhousie</h3>
                                 </div>
@@ -109,10 +159,32 @@ require('includes/header.php');
                         </div>
                         <div class="col">
                             <div class="card">
+                                <img src="assets/images/Kinnaur.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h3 class="card-title text-center h-font">Kinnaur</h3>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container my-5">
+        <div class="card shadow-lg">
+                <div class="row g-0">
+                    <div class="col-sm-6 mx-auto col-xl-5 order-2 order-xl-1">
+                        <img src="assets/images/MountainGalaxy.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-xl-7 order-1 order-xl-2">
+                        <div class="card-body">
+                            <p class="h4 h-font text-center">Comfort and Luxury Await at <span class="text-success">Our Manali Hotel</span></p>
+                            <h2 class="h1 h-font text-center text-primary">Mountain Galaxy</h2>
+                            <p class="h4 h-font text-center text-warning mb-4"><i class="bi bi-star-fill me-1"></i><i class="bi bi-star-fill me-1"></i><i class="bi bi-star-fill me-1"></i><i class="bi bi-star-fill me-1"></i><i class="bi bi-star-fill"></i></p>
+                            <p class="card-text text-center">Stay in comfort and luxury at our exquisite hotel in Manali. Nestled amidst the breathtaking beauty of the Himalayas, our hotel offers a tranquil retreat with well-appointed rooms, stunning views, and warm hospitality. Experience the perfect blend of modern amenities and the serene charm of nature during your stay with us.</p>
+                            <p class="text-center mt-5"><a href="" class="btn btn-lg btn-outline-success rounded-pill">Hotel Website</a></p>
                         </div>
                     </div>
                 </div>
